fbda1f5b17a999b5d0eccdeb718c551f759cda69
BlaBlaPer <WsHan9527@gmail.com>
2017-12-06 11:14:25 +0000

    Weisi update

--- db_func.py
+++ db_func.py
@@ -16,7 +16,7 @@ def db_get_input_csv():
 def db_get_input():
     sample_questions=[]
     session_id="0"
-    db = pymysql.connect("localhost", "root", "weisi9527sj", "P4BA")
+    db = pymysql.connect("localhost", "root", "weisi9527sj", "P5BA")
     cursor = db.cursor()
     
     try:
@@ -24,10 +24,15 @@ def db_get_input():
         cursor.execute(sql)
         results = cursor.fetchall()[0]
         session_id = str(results[0]+1)
+
+        sessionTimestamp =str(int(time.time()*1000))
+        print("p---------------------")
+        sql_sess = "INSERT INTO Session(sessionID, sessionTimestamp) VALUES ("+session_id+","+sessionTimestamp+")"
+
         
-        sql_n = "INSERT INTO Session(sessionID) VALUES ("+session_id+")"
-        cursor.execute(sql_n)
-        
+        print(sql_sess)
+        cursor.execute(sql_sess)
+
         
         sql_question = "select * from Question"
         cursor.execute(sql_question)
@@ -40,20 +45,23 @@ def db_get_input():
             question = questions_table[q_id][1]
             sample_questions = sample_questions + [question]
             q_id_real =str(q_id+1)
-            
-            sql_sq =  "INSERT INTO Session_question(sessionID,questionID) VALUES ("+session_id+","+q_id_real+")"
-            cursor.execute(sql_sq)
             db.commit()
 
     except:
         # 如果发生错误则回滚
         db.rollback()
-        print("sad.......")
+        print("sad_ques")
         db.close()
     return session_id,sample_questions
 
 # Process the results from index.html
 def db_store_results(session_id,log_affdex,log_xlabs,log_events):
+    
+    #------------event-----------------
+    
+    
+    
+    
     #print("I received data from the session_id=",session_id)
     return
 
